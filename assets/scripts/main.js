81084d3d58410f46c954c6f79f9326d997ae5cfc
Jordan LAT <jordanlat@hotmail.fr>
2019-10-24 22:21:01 +0200

    Fix responsiv - debut de solution javascrpt

---
+++ assets/scripts/main.js
@@ -0,0 +1,38 @@
+function mixingPeople() {
+    const Madina = new Boolean("false");
+    const Mouafak = new Boolean("false");
+    const Edward = new Boolean("false");
+    const Damien = new Boolean("false");
+    const Thibau = new Boolean("false");
+    const Joël = new Boolean("false");
+    const Déborah = new Boolean("false");
+    const Anaïs = new Boolean("false");
+    const Allan = new Boolean("false");
+    const Justin = new Boolean("false");
+    const Victoria = new Boolean("false");
+    const Kévin = new Boolean("false");
+    const Antonin = new Boolean("false");
+    const LucaR = new Boolean("false");
+    const LucaB = new Boolean("false");
+    const Medhi = new Boolean("false");
+    const Nathan = new Boolean("false");
+    const Tom = new Boolean("false");
+    const Jordan = new Boolean("false");
+    const Florent = new Boolean("false");
+    const Eleve1 = new Boolean("false");
+    const Eleve2 = new Boolean("false");
+    const Eleve3 = new Boolean("false");
+    const Eleve4 = new Boolean("false");
+    const Eleve5 = new Boolean("false");
+    const Eleve6 = new Boolean("false");
+    const Eleve7 = new Boolean("false");
+    const Joker = new Boolean("false");
+
+    var eleves = ["Madina","Mouafak","Edward","Damien","Thibau","Joël","Déborah","Anaïs","Justin","Victoria","Kévin","Allan","Antonin","Luca.R","Luca.B","Medhi","Nathan","Tom","Jordan","Florent","Eleve 1","Eleve 2","Eleve 3","Eleve 4","Eleve 5","Eleve 6","Eleve 7", "Joker"];
+    var check = [Madina];
+
+    let newDiv = document.createElement("div");
+    let newContent = document.createTextNode(eleves[1]);
+    newDiv.appendChild(newContent);
+    console.log("Eleve : "+eleves[0]);
+}
