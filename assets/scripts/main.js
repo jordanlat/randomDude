99c559bc37f300639866ae71bb696e7690901d33
Jordan LAT <jordanlat@hotmail.fr>
2019-10-27 10:26:54 +0100

    réduction et opti code + ajout fonction qui exec le code une seule fois

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,51 +1,54 @@
+function execMixingPeople(){
+    if(document.getElementsByClassName('prenom').length==0){
+        mixingPeople();
+    }
+}
+
+
+
 function mixingPeople() {
-    let Madina = new Boolean(false);
-    let Mouafak = new Boolean(false);
-    let Edward = new Boolean(false);
-    let Damien = new Boolean(false);
-    let Thibau = new Boolean(false);
-    let Joël = new Boolean(false);
-    let Déborah = new Boolean(false);
-    let Anaïs = new Boolean(false);
-    let Allan = new Boolean(false);
-    let Justin = new Boolean(false);
-    let Victoria = new Boolean(false);
-    let Kévin = new Boolean(false);
-    let Antonin = new Boolean(false);
-    let LucaR = new Boolean(false);
-    let LucaB = new Boolean(false);
-    let Medhi = new Boolean(false);
-    let Nathan = new Boolean(false);
-    let Tom = new Boolean(false);
-    let Jordan = new Boolean(false);
-    let Florent = new Boolean(false);
-    let Eleve1 = new Boolean(false);
-    let Eleve2 = new Boolean(false);
-    let Eleve3 = new Boolean(false);
-    let Eleve4 = new Boolean(false);
-    let Cindy = new Boolean(false);
-    let Jeremy = new Boolean(false);
-    let Naïm = new Boolean(false);
-    let Joker = new Boolean(false);
+    //On crée deux tableau, le premier contient les prénoms au format text,
+    let elevesArray =
+     ["Madina","Mouafak","Edward","Damien","Thibau","Joël","Déborah","Anaïs",
+     "Justin","Victoria","Kévin","Allan","Antonin","Luca.R","Luca.B","Medhi",
+     "Nathan","Tom","Jordan","Florent","Eleve 1","Eleve 2","Eleve 3","Eleve 4",
+     "Cindy","Jeremy","Naïm", "Joker"];
+    //  et le second contient des valeurs booleen.
+    let checkArray =
+     [Madina=false,Mouafak=false,Edward=false,Damien=false,Thibau=false,Joël=false,
+     Déborah=false,Anaïs=false,Justin=false,Victoria=false,Kévin=false,Allan=false,
+     Antonin=false,LucaB=false,LucaR=false,Medhi=false,Nathan=false,Tom=false,Jordan=false,
+     Florent=false,Cindy=false,Jeremy=false,Naïm=false,Eleve1=false,Eleve2=false,Eleve3=false,
+     Eleve4=false,Joker=false];
 
-    let elevesArray = ["Madina","Mouafak","Edward","Damien","Thibau","Joël","Déborah","Anaïs","Justin","Victoria","Kévin","Allan","Antonin","Luca.R","Luca.B","Medhi","Nathan","Tom","Jordan","Florent","Eleve 1","Eleve 2","Eleve 3","Eleve 4","Cindy","Jeremy","Naïm", "Joker"];
-    let checkArray = [Madina,Mouafak,Edward,Damien,Thibau,Joël,Déborah,Anaïs,Justin,Victoria,Kévin,Allan,Antonin,LucaB,LucaR,Medhi,Nathan,Tom,Jordan,Florent,Cindy,Jeremy,Naïm,Eleve1,Eleve2,Eleve3,Eleve4,Joker];
+    let numBoolRCounter = 0; //variable pour compter le nombre de bool à vrai.
+    let numberDiv = 0; //variable pour compter le nombre de Div.
 
-    let numBoolFCounter = 0;
-
-    while(numBoolFCounter<elevesArray.length){
+    //Ci-dessous: tant que le compteur de bool vrai est inférieur au nombre d'entré dans le tableau élèves,
+    // on éxecute le code suivant.
+    while(numBoolRCounter<elevesArray.length){
+        //Pour i valent 0, si i est inférieur au nombre d'entrés, dans le tableau élèves, alors j'incrémente i.
         for(let i=0;i<elevesArray.length;i++){
-            let rNumber = Math.floor(Math.random()*(elevesArray.length));
-            
+            let rNumber = Math.floor(Math.random()*(elevesArray.length)); // On crée un nombre aléatoire arrondie au supérieur.
             if(checkArray[rNumber]==false){
+                numberDiv++;
+                if(numberDiv%2 == 1){
+                    let createHr = document.createElement("hr");
+                    let createH3 = document.createElement("h3");
+                    let createGrpName = document.createTextNode("Groupe n° "+(Math.floor(numberDiv/2)+1));
+                    createH3.appendChild(createGrpName);
+                    document.getElementById('content').appendChild(createHr);
+                    document.getElementById('content').appendChild(createH3);
+                }
                 let createDiv = document.createElement("div");
+                createDiv.classList.add('prenom');
                 let divContent = document.createTextNode(elevesArray[rNumber]);
                 createDiv.appendChild(divContent);
                 document.getElementById("content").appendChild(createDiv);
                 checkArray[rNumber]=true;
+
             }
         }
-        numBoolFCounter+=1;
-        console.log(numBoolFCounter);
+        numBoolRCounter+=1;
     }
 }
