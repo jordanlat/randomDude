8f8c179c502de1fe8d621ed871b7f81db84fb7f6
Jordan LAT <jordanlat@hotmail.fr>
2019-11-02 10:12:39 +0100

    ajout commentaire

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,37 +1,45 @@
 function tinder(){
-    let elevesArray =
+    //Je déclare deux tableau, l'un rempli de nom, l'autre vide
+    const elevesArray =
     ["Madina","Mouafak","Edward","Damien","Thibau","Joël","Déborah","Anaïs",
     "Justin","Victoria","Kévin","Allan","Antonin","Luca.R","Luca.B","Medhi",
     "Nathan","Jordan","Erwan","Cindy","Jeremy","Naïm"];  
     let newArray = [];
+    //Si le tableau est impaire, alors j'ajoute joker
+    if(elevesArray.length%2==1){elevesArray.push("Joker");}
+
     for(let i=elevesArray.length;i>=0;i--){
         let rNumber = Math.floor(Math.random()*(elevesArray.length));
         newArray.push(elevesArray[rNumber]);
         let suppr = elevesArray.indexOf(elevesArray[rNumber]);
         elevesArray.splice(suppr,1);  
     }
-    function addStructure(){
+/* -- Function createStructure -- */
+        //Je déclare mes variables, pour cette fonction
         let halfClass = Math.floor(newArray.length/2);
         let nbrP = 0;
-        let n = 1;
-
-        let getList = document.getElementById('intro');
-        let addHR = document.createElement("hr");
-        //addHR.classList.add("flexCenter");
-        getList.appendChild(addHR);
-        
+        let nbrGrp = 1;
+        //J'ajoute une balise HR si elle n'existe pas
+        if(document.getElementsByTagName('hr').length==0){
+            let getList = document.getElementById('intro');
+            let addHR = document.createElement("hr");
+            getList.appendChild(addHR);
+        }
+        /*
+            Je crée une div 'group' dans laquelle j'ajoute un titre et 2 'p'
+        */
         if(document.getElementsByClassName("group").length==0){
             for (let i=0;i<halfClass;i++){
-                
+                // création d'un div
                 let addH = document.createElement("div");
                 addH.classList.add("group");
                 document.getElementById('list').appendChild(addH);
-
+                // ajout du titre
                 let addTitre = document.createElement("h3");
-                let addTextTitre = document.createTextNode("Groupe "+n);
+                let addTextTitre = document.createTextNode("Groupe "+nbrGrp);
                 addTitre.appendChild(addTextTitre);
                 addH.appendChild(addTitre);
-
+                // ajout des 'p'
                 for(let i=0;i<2;i++){
                     let addP = document.createElement("p");
                     addP.setAttribute("id","group"+nbrP);
@@ -40,18 +48,15 @@ function tinder(){
                     addH.appendChild(addP);
                     nbrP++;
                 }
-                n++;
+                nbrGrp++;
             }
         }
-    }
-    addStructure();
-    function insertNameIntoP(){
+/* -- Function insertName -- */
+// pour chaque entré de newArray, on va vider le 'p' puis remplacer la valeur dans newArray
         for(let i=0; i<newArray.length;i++){
-            document.getElementById("group"+i).innerHTML="";
+            document.getElementById("group"+i).innerHTML=" ";
             let target = document.getElementById("group"+i).innerHTML;
             let update = target.replace("",newArray[i]);
             document.getElementById("group"+i).innerHTML = update;
         }
-    }
-    insertNameIntoP();
 }
