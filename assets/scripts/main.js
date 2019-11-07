d670fc121e7ba38d7e85fbef71e58cd80fb44604
Jordan LAT <jordanlat@hotmail.fr>
2019-11-07 23:01:35 +0100

    ajout bp nbr person dans un group

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,16 @@
 function tinder(){
+    // -- Je récupère le nombre de personne dans le groupe --
+    let nbrPeople = document.getElementById("nbrMate").value;
     //Je déclare deux tableau, l'un rempli de nom, l'autre vide
     const elevesArray =
     ["Madina","Mouafak","Edward","Damien","Thibau","Joël","Déborah","Anaïs",
     "Justin","Victoria","Kévin","Allan","Antonin","Luca.R","Luca.B","Medhi",
     "Nathan","Jordan","Erwan","Cindy","Jeremy","Naïm"];  
     let newArray = [];
-    //Si le tableau est impaire, alors j'ajoute joker
+    //-- Je détermine le nombre de groupe --
+    let nbrGrp = Math.ceil(elevesArray.length / nbrPeople);
+    //console.log(nbrGrp);
+    // --Si le tableau est impaire, alors j'ajoute joker --
     if(elevesArray.length%2==1){elevesArray.push("Joker");}
 /* -- Function createNewArray -- */
     for(let i=elevesArray.length;i>=0;i--){
@@ -14,12 +19,17 @@ function tinder(){
         let suppr = elevesArray.indexOf(elevesArray[rNumber]);
         elevesArray.splice(suppr,1);  
     }
+/* -- Function remove child -- */
+    //console.log(document.getElementsByTagName('p').length);
+    if(document.getElementsByTagName('p').length > 0){
+        document.getElementById('list').innerHTML = "";
+    }
 /* -- Function createStructure -- */
-        //Je déclare mes variables, pour cette fonction
+        // -- Je déclare mes variables, pour cette fonction --
         let halfClass = Math.floor(newArray.length/2);
         let nbrP = 0;
-        let nbrGrp = 1;
-        //J'ajoute une balise HR si elle n'existe pas
+        let grpCounter = 1;
+        // -- J'ajoute une balise HR si elle n'existe pas --
         if(document.getElementsByTagName('hr').length==0){
             let getList = document.getElementById('intro');
             let addHR = document.createElement("hr");
@@ -29,18 +39,19 @@ function tinder(){
             Je crée une div 'group' dans laquelle j'ajoute un titre et 2 'p'
         */
         if(document.getElementsByClassName("group").length==0){
-            for (let i=0;i<halfClass;i++){
+            for (let i=0;i<nbrGrp;i++){
                 // création d'un div
                 let addH = document.createElement("div");
                 addH.classList.add("group");
                 document.getElementById('list').appendChild(addH);
                 // ajout du titre
                 let addTitre = document.createElement("h3");
-                let addTextTitre = document.createTextNode("Groupe "+nbrGrp);
+                let addTextTitre = document.createTextNode("Groupe "+grpCounter);
                 addTitre.appendChild(addTextTitre);
                 addH.appendChild(addTitre);
                 // ajout des 'p'
-                for(let i=0;i<2;i++){
+                //for(let i=0;i<2;i++){
+                for(let i=0;i<nbrPeople;i++){
                     let addP = document.createElement("p");
                     addP.setAttribute("id","group"+nbrP);
                     let addTextP = document.createTextNode("");
@@ -48,17 +59,19 @@ function tinder(){
                     addH.appendChild(addP);
                     nbrP++;
                 }
-                nbrGrp++;
+                grpCounter++;
             }
         }
 /* -- Function insertName -- */
-// pour chaque entré de newArray, on va vider le 'p' puis remplacer la valeur dans newArray
+// -- pour chaque entré de newArray, on va vider le 'p' puis remplacer la valeur dans newArray --
         for(let i=0; i<newArray.length;i++){
-            document.getElementById("group"+i).innerHTML=" ";
+            document.getElementById("group"+i).innerHTML= "";
             let target = document.getElementById("group"+i).innerHTML;
+            //let target = document.getElementById("group"+i);
             let update = target.replace("",newArray[i]);
             document.getElementById("group"+i).innerHTML = update;
         }
+    console.log("fin");
 }
 
 /* -- Scroll to Top BP -- */
@@ -70,15 +83,16 @@ function hideSeek(){
     /*console.log(scrollStatus);*/
     if(scrollStatus > 700){
         backToTopBP.style.display = "block";
-        console.log("hein?!");
-
+        //console.log("hein?!");
+        
         if(backToTopBP.style.display == "block"){
-            console.log("j'ai tendance à bloquer"); 
+            //console.log("j'ai tendance à bloquer"); 
             /*
             backToTopBP.style.transition = "opacity 3s";
             */
            backToTopBP.style.display = "block";
         }
+        
         /*
 
 
@@ -95,9 +109,15 @@ function hideSeek(){
         transition-duration: 4s;
         transition-delay: 2s;
         */
+       /*
+        const getBp = document.getElementById('idTopBp');
+        getBp.style.opacity = 1;
+        getBp.style.transition = "opacity 3s";
+        console.log('transition');
+        */
     } else {
         backToTopBP.style.display = "none";
-        console.log("Je ne bloque plus, merci");
+        //console.log("Je ne bloque plus, merci");
     }
 }
 
